a1203714460358885b49dfb874ddfe1ae1e915b5
conorhastings <hastings.conorm@gmail.com>
2015-12-07 00:35:37 -0500

    add ability for user to handle error in their own way while providing a default error handler the default handler provides the same functionality as current module

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
-export default function reduxUnhandledAction() {
+import defaultErrorHandler from "./default-error-handler";
+
+export default function reduxUnhandledAction(callback = defaultErrorHandler) {
 	return ({getState}) => (next) => (action) => {
 		const prevState = getState();
 		const result = next(action);
 		const nextState = getState();
 		if (prevState === nextState) {
-			console.error(`action with type ${action.type} did not modify the data in any reducer.` +
-				`You may have mutated state directly or you may have a typo in your reducer`);
+			callback(action);
 		}
 		return result;
 	};
