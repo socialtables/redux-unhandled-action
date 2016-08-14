07717825ba1cb9e9e88119fce40ccc060e29d093
Amit Singla <singla.amit1911@gmail.com>
2016-08-14 18:34:18 +0800

    added action validation

--- src/index.js
+++ src/index.js
@@ -1,14 +1,19 @@
 import defaultErrorHandler from "./default-error-handler";
+import isPlainObject from "lodash.isplainobject";
 
 export default function reduxUnhandledAction(callback = defaultErrorHandler) {
-	return ({getState}) => (next) => (action) => {
-		const prevState = getState();
-		const result = next(action);
-		const nextState = getState();
-		if (prevState === nextState) {
-			callback(action);
-		}
-		return result;
-	};
-
+    return ({ getState }) => (next) => (action) => {
+        if (isPlainObject(action)) {
+            if (typeof action.type !== 'undefined') {
+                const prevState = getState();
+                const result = next(action);
+                const nextState = getState();
+                if (prevState === nextState) {
+                    callback(action);
+                }
+                return result;
+            }
+        }
+        return next(action);
+    };
 }
