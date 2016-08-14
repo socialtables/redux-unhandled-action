0d4acf733097a3f9e857363cedbc7723711013de
Amit Singla <singla.amit1911@gmail.com>
2016-08-15 02:23:40 +0800

    replaced nested if statements with single line if statement.

--- src/index.js
+++ src/index.js
@@ -3,16 +3,14 @@ import isPlainObject from "lodash.isplainobject";
 
 export default function reduxUnhandledAction(callback = defaultErrorHandler) {
     return ({ getState }) => (next) => (action) => {
-        if (isPlainObject(action)) {
-            if (typeof action.type !== 'undefined') {
-                const prevState = getState();
-                const result = next(action);
-                const nextState = getState();
-                if (prevState === nextState) {
-                    callback(action);
-                }
-                return result;
+        if (isPlainObject(action) && typeof action.type !== "undefined") {
+            const prevState = getState();
+            const result = next(action);
+            const nextState = getState();
+            if (prevState === nextState) {
+                callback(action);
             }
+            return result;
         }
         return next(action);
     };
